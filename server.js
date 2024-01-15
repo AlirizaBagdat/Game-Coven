15367295b18b92f2b062caebb3b7e67a060a0047
rlobz <rlobzowski@gmail.com>
2024-01-15 21:27:53 +0000

    installed packages and structure

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+const sess = {
+  secret: 'Your secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
