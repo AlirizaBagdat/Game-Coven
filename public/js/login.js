51dfb8ec3a7afa0ed32914a301a1a32b849c06e5
Ali K <aliekx609@gmail.com>
2024-01-18 21:20:40 +0000

    creation of login.jswith comments to assist.

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,23 @@
 const login = async (event) => {
-    event.preventDefault();
+    event.preventDefault();                                                               // <= Preventing the default form submission behavior.
 
-    const email = document.querySelector('#emailLogin').value.trim();                           // <= queryid needs to be created for #
-    const password = document.querySelector('#passwordLogin').value.trim();                     // <= queryid needs to be created for #
+    const email = document.querySelector('#emailLogin').value.trim();                     // <= queryid needs to be created for #
+    const password = document.querySelector('#passwordLogin').value.trim();               // <= queryid needs to be created for #
 
     if (email && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/users/login', {                                // <= Checking if both email and password are provided. Sends a POST request to the endpoint.
             method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
-        if (response.ok) {
-            console.log('Logged in successfully!');
-            document.location.replace('/');                  // <= need to enter route here after login 
+        if (response.ok) {                                                                // <= Checking if the response status is ok.
+            console.log('Logged in successfully!');                                       // <= If login is successful, then logs a message to user and redirect to the home page.
+            document.location.replace('/');        // <= Need to enter route here!                                        
         } else {
             alert(response.statusText);
         }
     }
 }
 
-document.querySelector('#loginBtn').addEventListener('submit', login);
+document.querySelector('#loginBtn').addEventListener('submit', login);                    
