86ddc79bc17ad2e1d26dff8bda8fc37b6bcc7df3
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2024-01-20 16:36:54 +0000

    controllers has been completed

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+// Import the necessary dependencies
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+
+// Use the homeRoutes for the root path "/"
+router.use("/", homeRoutes);
+
+// Use the apiRoutes for the "/api" path
+router.use("/api", apiRoutes);
+
+// Export the router
+module.exports = router;
