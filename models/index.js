7b4f18db5ac562a943aac09adc820f8046519d24
mutasim chaudhary <mutasim.chaud@gmail.com>
2024-01-25 14:33:18 +0000

    new package added

--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
 const User = require('./User');
 const Review = require('./Review');
 const Game = require('./Game')
-const Comment = require('./Comment')
-
 User.hasMany(Review, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
@@ -12,4 +10,4 @@ Review.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Review, Game, Comment };
+module.exports = { User, Review, Game };
