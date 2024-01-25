a13c670545aacfe26e450c28600548eef74276b3
rlobz <rlobzowski@gmail.com>
2024-01-25 17:06:16 +0000

    jobs some done

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const path = require('path');
 const express = require('express');
+const app = express();
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-//const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 // Set up Handlebars.js engine with custom helpers
 
 const sess = {
